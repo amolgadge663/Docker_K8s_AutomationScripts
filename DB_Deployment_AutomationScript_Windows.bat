da4318b75be3e410aee275af527fd76060918428
Amol Kailas Gadage <amol.gadage@incedoinc.com>
2023-04-28 11:15:08 +0530

    code reformat

--- DB_Deployment_AutomationScript_Windows.bat
+++ DB_Deployment_AutomationScript_Windows.bat
@@ -1,32 +1,39 @@
 @echo off
-echo Starting Minikube...
+REM This script automates the following commands:
+REM - Start a minikube node using Docker driver
+REM - Check the minikube status
+REM - Connect minikube and docker
+REM - Create a deployment using a YAML file
+REM - Get all the deployments
+REM - Get all the pods and store the name of one pod
+REM - Wait for 1 minute
+REM - Get the logs of the stored pod
+REM - Get the description of the stored pod
+
+REM Start a minikube node using Docker driver
 minikube start --driver=docker
-echo.
-echo Checking Minikube status...
+
+REM Check the minikube status
 minikube status
-echo.
-echo Connecting to Minikube's Docker daemon...
-@FOR /f "tokens=*" %%i IN ('minikube docker-env') DO @%%i
-echo.
-echo Running the deployment...
-kubectl apply -f db_deployment.yml
-echo.
-echo Checking the deployment...
-kubectl get deployment
-echo.
-echo Checking the pods...
-setlocal EnableDelayedExpansion
-set pod_name=
-for /f "skip=1 delims=" %%i in ('kubectl get pods') do (
-    if not defined pod_name (
-        set "pod_name=%%i"
-        set "pod_name=!pod_name:~0,12!"
-    )
-)
-echo The pod name is %pod_name%
-echo.
-echo Checking the logs...
+
+REM Connect minikube and docker
+minikube docker-env | Invoke-Expression
+
+REM Create a deployment using a YAML file
+kubectl apply -f db-deployment.yml
+
+REM Get all the deployments
+kubectl get deployments
+
+REM Get all the pods and store the name of one pod
+kubectl get pods
+set /p pod_name="Enter the name of the pod to get logs: "
+
+REM Wait for 1 minute
+timeout /t 60
+
+REM Get the logs of the stored pod
 kubectl logs %pod_name%
-echo.
-echo Script completed successfully.
-pause
+
+REM Get the description of the stored pod
+kubectl describe pod %pod_name%
